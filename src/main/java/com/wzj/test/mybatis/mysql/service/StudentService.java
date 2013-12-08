3dd5a63cb28b6e025b61bd4c9bdc1c041b18fc53
cat <wzjhz88@163.com>
2013-12-08 19:05:50 +0800

    spring cxf mybatis成功发布了一个web service

--- src/main/java/com/wzj/test/mybatis/mysql/service/StudentService.java
+++ src/main/java/com/wzj/test/mybatis/mysql/service/StudentService.java
@@ -1,5 +1,15 @@
 package com.wzj.test.mybatis.mysql.service;
 
-public class StudentService {
+import java.util.List;
+
+import com.wzj.test.mybatis.mysql.entity.StudentBean;
+
+public interface StudentService {
+	public void addStudents(List<StudentBean> student);
     
+    public void updateStudent(StudentBean student);
+    
+    public List<StudentBean> getStudents(List<String> IDs);
+    
+    public void deleteStudents(List<String> IDs);
 }
